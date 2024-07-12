ffc65939199ecedf6cbcb119d02a60177a14d98a
Lyndsay Vance <lyndsayvance@Lyndsays-MBP.attlocal.net>
2024-07-11 18:38:56 -0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(puppies)
   
   return (
-    <> Hello World
-    </>
+    <div>
+      <div className = 'featured-pup-container'>
+        {featuredPup && (
+          <div className = 'featured-pup-details'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <div className = 'puppy-list'>
+        {puppies.map((puppy) => (
+          <p className='puppy-name'
+            key={puppy.id}
+            onClick={()=> {setFeatPupId(puppy.id)}}
+          >
+            {puppy.name}
+          </p>
+        ))}
+      </div>
+    </div>
   )
 }
 
